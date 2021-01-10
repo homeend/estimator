79d4725058a30f2277dada0a532a2a503e4cd577
Michal R <homeend@wp.pl>
2021-01-10 22:02:17 +0100

    added basic table logic

--- src/Table.svelte
+++ src/Table.svelte
@@ -1,1 +1,47 @@
-<h1>table</h1>
+<script>
+    import my_store from './material-store.js';
+
+    let materials = [];
+    let cost = 0;
+
+    my_store.subscribe(items => {
+        materials = items;
+        cost = items.map(e => e.price).reduce((prev, next) => prev + next, 0);
+    });
+
+    const formatter = new Intl.NumberFormat('pl-pl', {style: "currency", currency: "PLN"});
+
+     
+
+</script>
+
+<style>
+    table {
+        width: 100%;
+    }
+</style>
+
+<table class="primary">
+    <thead>
+        <tr>
+            <th>Material</th>
+            <th>Price</th>
+            <th/>
+        </tr>
+    </thead>
+    <tbody>
+        {#each materials as material (material.id)}
+        <tr>
+            <td>{material.name}</td>
+            <td>{formatter.format(material.price)}</td>
+            <td><i class="far fa-trash-alt" /></td>
+        </tr>
+        {/each}
+        {#if materials.length>0}
+        <tr>
+            <td>Total cost:</td>
+            <td rowspan="2">{formatter.format(cost)}</td>
+        </tr>
+        {/if}
+    </tbody>
+</table>
